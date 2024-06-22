5f24beeab2f370df95aeb8473be5f80d9c813e5b
Mezeru7 <okolelov.ivan@gmail.com>
2024-06-22 17:26:06 +0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {LoginComponent} from "../login/login.component";
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: AppComponent }, // Перенаправление на страницу входа
+  { path: 'login', component: LoginComponent }, // Страница входа
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
